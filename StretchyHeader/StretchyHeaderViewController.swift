e2c7051eb74e27ba0e3d454edf54e0344a81b5ca
Leisan Latypova <latley90@gmail.com>
2022-11-11 17:47:20 +0300

    Initial commit

---
+++ StretchyHeader/StretchyHeaderViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderViewController.swift
+//  StretchyHeader
+//
+//  Created by Leysan Latypova on 11.11.2022.
+//
+
+import UIKit
+
+class StretchyHeaderViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .yellow
+    }
+}
