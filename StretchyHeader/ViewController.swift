b802c25f6145e3e5353ad0879aa28bad268e7f22
Leisan Latypova <latley90@gmail.com>
2022-11-11 12:49:05 +0300

    Initial Commit

---
+++ StretchyHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by Leysan Latypova on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
